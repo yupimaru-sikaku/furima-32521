6efd8115bf288a02d5e3ea420e49ab558bfee512
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2020-11-30 13:28:32 +0900

    #7_商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def create
     @item  = Item.new(item_params)
     if @item.valid? 
