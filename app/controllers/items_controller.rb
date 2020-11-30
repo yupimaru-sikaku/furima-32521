cd11378611bad3a8ebb5daf41f5fb4574a1ad315
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2020-11-30 15:07:49 +0900

    #8_商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -19,6 +19,23 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+      @item = Item.find(params[:id])
+      unless @item.user.id == current_user.id
+        redirect_to root_path
+      end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(@item)
+    else
+      render :edit
     end
   end
 
